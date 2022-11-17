2464e6da90fb33d4e38a1aa80b21c4201103322b
Acer <Acer@10.10.217.197>
2022-11-17 10:59:59 +0100

    Playground

--- Swp_3_Klasse_sagmeister/src/Schiffe_versenken/Playground.java
+++ Swp_3_Klasse_sagmeister/src/Schiffe_versenken/Playground.java
@@ -2,6 +2,27 @@ package Schiffe_versenken;
 
 public class Playground {
 
+	static private int [][] spielFeld;
 	
+	static void baseField() {
+        spielFeld = new int[10][10]; 
+        for (int i = 0; i < spielFeld.length; i++) {
+            for (int j = 0; j < spielFeld[i].length; j++) {
+                spielFeld[i][j] = 0;
+            }
+        }
+    }
+	public static void main(String[] args) {
+		baseField();
+		showField();
+	}
+	static void showField() {
+        for (int i = 0; i < spielFeld.length; i++) {
+            for (int j = 0; j < spielFeld[i].length; j++) {
+                System.out.printf(" %d",spielFeld[i][j]);
+            }
+            System.out.println();
+        }
+    }
 	
 }
